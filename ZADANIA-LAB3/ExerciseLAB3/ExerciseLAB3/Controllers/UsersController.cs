1c5c22bed35abc3330a86aec03c3341be4168540
dominik1408 <79981778+dominik1408@users.noreply.github.com>
2022-05-15 13:15:46 +0200

    exercise lab3

---
+++ ZADANIA-LAB3/ExerciseLAB3/ExerciseLAB3/Controllers/UsersController.cs
@@ -0,0 +1,62 @@
+﻿using ExerciseLAB3.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExerciseLAB3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public UsersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        //GET: api/user
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUser()
+        {
+            var users = _context.Users;
+            return await users.ToListAsync();
+        }
+
+        //GET: api/user/{id}
+        [HttpGet("{id}")]
+        public async Task<ActionResult<IEnumerable<User>>> GetUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound("User don't exist");
+            }
+            return Ok(user);
+
+        }
+
+        //POST: api/user
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(User user)
+        {
+            var newUser = new User
+            {
+                name = user.name,
+                email = user.email
+            };
+
+            _context.Users.Add(newUser);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(
+            nameof(GetUser), new {id = user.id},
+            newUser);
+        }
+    }
+}
---
+++ ZADANIA-LAB3/ExerciseLAB3/ExerciseLAB3/User.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExerciseLAB3
+{
+    public class User
+    {
+        [Key]
+        public int id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+    }
+}
