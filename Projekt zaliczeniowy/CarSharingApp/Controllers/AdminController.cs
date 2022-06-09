ac00eb6737af742681d2590c7742305a4d980b37
dominik1408 <79981778+dominik1408@users.noreply.github.com>
2022-06-09 19:36:42 +0200

    Created controllers for Admin, User, Loan, LoanType models

---
+++ Projekt zaliczeniowy/CarSharingApp/Controllers/AdminController.cs
@@ -0,0 +1,106 @@
+﻿using CarSharingApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarSharingApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public AdminController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Admin>>> GetAdmin()
+        {
+            return await _context.Admins.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Admin>> GetAdminById(int id)
+        {
+            var admin = await _context.Admins.FindAsync(id);
+
+            if(admin == null)
+            {
+                return NotFound();
+            }
+            return Ok(admin);
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Admin>> PostAdmin(Admin admin)
+        {
+            var adminModel = new Admin
+            {
+                UserId = admin.UserId
+            };
+
+            _context.Admins.Add(adminModel);
+            await _context.SaveChangesAsync();
+
+           return CreatedAtAction(nameof(GetAdmin),
+                new { id = adminModel.AdminId },
+                admin);
+        }
+
+        
+        [HttpPut("id")]
+        public async Task<ActionResult<Admin>> PutAdmin(int id, Admin admin)
+        {
+
+            if (id != admin.AdminId)
+            {
+                return BadRequest();
+            }
+
+            var adminModel = await _context.Admins.FindAsync(id);
+            if (adminModel == null)
+            {
+                return NotFound();
+            }
+
+            adminModel.UserId = admin.UserId;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException) when (!AdminExists(id))
+            {
+
+                return NotFound();
+            }
+
+            return Ok(adminModel);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteAdmin(int id)
+        {
+            var admin = await _context.Admins.FindAsync(id);
+
+            if (admin == null)
+            {
+                return NotFound();
+            }
+
+            _context.Admins.Remove(admin);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool AdminExists(int id)
+        {
+            return _context.Admins.Any(e => e.AdminId == id);
+        }
+
+    }
+}
---
+++ Projekt zaliczeniowy/CarSharingApp/Controllers/LoanTypesController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CarSharingApp.Data;
+using CarSharingApp.Models;
+
+namespace CarSharingApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoanTypesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public LoanTypesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/LoanTypes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<LoanType>>> GetLoanTypes()
+        {
+          if (_context.LoanTypes == null)
+          {
+              return NotFound();
+          }
+            return await _context.LoanTypes.ToListAsync();
+        }
+
+        // GET: api/LoanTypes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<LoanType>> GetLoanType(int id)
+        {
+          if (_context.LoanTypes == null)
+          {
+              return NotFound();
+          }
+            var loanType = await _context.LoanTypes.FindAsync(id);
+
+            if (loanType == null)
+            {
+                return NotFound();
+            }
+
+            return loanType;
+        }
+
+        // PUT: api/LoanTypes/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutLoanType(int id, LoanType loanType)
+        {
+            if (id != loanType.LoanTypeId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(loanType).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!LoanTypeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/LoanTypes
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<LoanType>> PostLoanType(LoanType loanType)
+        {
+          if (_context.LoanTypes == null)
+          {
+              return Problem("Entity set 'AppDbContext.LoanTypes'  is null.");
+          }
+            _context.LoanTypes.Add(loanType);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetLoanType", new { id = loanType.LoanTypeId }, loanType);
+        }
+
+        // DELETE: api/LoanTypes/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteLoanType(int id)
+        {
+            if (_context.LoanTypes == null)
+            {
+                return NotFound();
+            }
+            var loanType = await _context.LoanTypes.FindAsync(id);
+            if (loanType == null)
+            {
+                return NotFound();
+            }
+
+            _context.LoanTypes.Remove(loanType);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool LoanTypeExists(int id)
+        {
+            return (_context.LoanTypes?.Any(e => e.LoanTypeId == id)).GetValueOrDefault();
+        }
+    }
+}
