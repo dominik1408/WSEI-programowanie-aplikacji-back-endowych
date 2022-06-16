3d7c3f39656908669a491a45935ff25b2e9d5af7
dominik1408 <79981778+dominik1408@users.noreply.github.com>
2022-06-16 14:17:35 +0200

    Added validation for Login in Email for Users model

---
+++ Projekt zaliczeniowy/CarSharingApp/Validation/EmailValidation.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarSharingApp.Validation
+{
+    public class EmailValidation : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            var _context = (AppDbContext)validationContext.GetService(typeof(AppDbContext));
+            var entity = _context.Users.SingleOrDefaultAsync(a => a.Email == value.ToString());
+            if(entity == null)
+            {
+                return new ValidationResult(GetErrorMesage(value.ToString()));
+            }
+            return ValidationResult.Success;
+
+        }
+
+        public string GetErrorMesage(string Email)
+        {
+            return $"{Email} is already in use";
+        }
+    }
+}
---
+++ Projekt zaliczeniowy/CarSharingApp/Validation/LoginValidation.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarSharingApp.Validation
+{
+    public class LoginValidation : ValidationAttribute 
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            var _context = (AppDbContext)validationContext.GetService(typeof(AppDbContext));
+            var entity = _context.Users.SingleOrDefaultAsync(a => a.Login == value.ToString());
+            if (entity == null)
+            {
+                return new ValidationResult(GetErrorMessage(value.ToString()));
+            }
+            return ValidationResult.Success;
+        }
+
+        public string GetErrorMessage(string Login)
+        {
+            return $"{Login} is already in use.";
+        }
+    }
+}
